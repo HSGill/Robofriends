aae8430d9f716890efc5df3339a91060027c4707
HSGill <harjotgill73@gmail.com>
2019-05-26 19:10:10 +1200

    Updates

---
+++ precache-manifest.d7c82d24f3a72466e2b896e2c72e5eb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/HSGill/Robofriends.git/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "71e6df242b30b872e8b2",
+    "url": "/HSGill/Robofriends.git/static/js/runtime~main.71e6df24.js"
+  },
+  {
+    "revision": "f8e73cd64907724273c9",
+    "url": "/HSGill/Robofriends.git/static/js/main.f8e73cd6.chunk.js"
+  },
+  {
+    "revision": "82a60734fd1b5c178473",
+    "url": "/HSGill/Robofriends.git/static/js/1.82a60734.chunk.js"
+  },
+  {
+    "revision": "f8e73cd64907724273c9",
+    "url": "/HSGill/Robofriends.git/static/css/main.1741847b.chunk.css"
+  },
+  {
+    "revision": "82a60734fd1b5c178473",
+    "url": "/HSGill/Robofriends.git/static/css/1.d2d23906.chunk.css"
+  },
+  {
+    "revision": "f81c6b288fbe7be7e0dbcd8adfd1827a",
+    "url": "/HSGill/Robofriends.git/index.html"
+  }
+];
